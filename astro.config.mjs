07a7875b323514bafbb11bc6e26042b978c65b92
DevJ25 <alavrezsanti@gmail.com>
2025-07-20 13:21:37 -0500

    Creacion de mi portafolio personal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
+});
